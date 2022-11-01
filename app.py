3590cc9330576b7cd24a790c0f7507b613852bd5
Sudhamsh Mondrati <smondra1@uncc.edu>
2022-11-01 19:14:23 -0400

    working prototype of login page, updated app and base navigation

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-import flask
+from flask import Flask, render_template, redirect
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
 	# TODO: Serve the landing page if the user isn't logged in
 
-	return flask.render_template("feed.html")
+	return render_template("feed.html")
 
 @app.route("/about")
 def about():
-	return flask.render_template("about.html")
+	return render_template("about.html")
 
 @app.route("/contact")
 def contact():
-	return flask.render_template("contact.html")
+	return render_template("contact.html")
 
 @app.route("/landing")
 def landing():
-	return flask.render_template("landing.html")
+	return render_template("landing.html")
+
+@app.get("/login")
+def login():
+	return render_template("login.html")
+
+@app.post("/login")
+def post_login():
+	return redirect('/feed')
