d04334d103ce52904d7cf0fa7ee1cdbc3955e728
jadenPete <jadenpete@github.com>
2022-11-15 21:45:03 -0500

    Added a database table for post tags and basic image retrieval

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,15 +1,22 @@
-for (const icon of document.getElementsByClassName("icon-togglable")) {
-	icon.addEventListener("mouseover", () => {
-		icon.classList.remove("far")
-		icon.classList.add("fa")
-	})
+function enableTogglableIcons() {
+	for (const icon of document.getElementsByClassName("icon-togglable")) {
+		icon.addEventListener("mouseover", () => {
+			icon.classList.remove("far")
+			icon.classList.add("fa")
+		})
 
-	icon.addEventListener("mouseout", () => {
-		icon.classList.remove("fa")
-		icon.classList.add("far")
-	})
+		icon.addEventListener("mouseout", () => {
+			icon.classList.remove("fa")
+			icon.classList.add("far")
+		})
+	}
 }
 
-for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-	new bootstrap.Tooltip(element)
+function enableTooltips() {
+	for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
+		new bootstrap.Tooltip(element)
+	}
 }
+
+enableTogglableIcons()
+enableTooltips()
